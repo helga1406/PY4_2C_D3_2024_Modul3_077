cde0ba38550f216f7f7bf2ae2209bec7e63bcad3
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2026-02-23 20:13:53 +0700

    hapus komentar

--- lib/features/widgets/log_item_widget.dart
+++ lib/features/widgets/log_item_widget.dart
@@ -15,21 +15,20 @@ class LogItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Widget ini hanya mengembalikan Card, bukan Scaffold atau App lengkap [cite: 49, 122]
     return Card(
       child: ListTile(
-        leading: const Icon(Icons.note), // Ikon catatan [cite: 105]
-        title: Text(log.title), // Menampilkan judul dari model [cite: 105]
-        subtitle: Text(log.description), // Menampilkan deskripsi dari model [cite: 105]
-        trailing: Wrap( // Langkah 5: Menggunakan Wrap untuk tombol aksi [cite: 121, 122]
+        leading: const Icon(Icons.note), 
+        title: Text(log.title), 
+        subtitle: Text(log.description), 
+        trailing: Wrap( 
           children: [
             IconButton(
               icon: const Icon(Icons.edit, color: Colors.blue),
-              onPressed: onEdit, // Memicu fungsi edit yang dikirim dari parent [cite: 122]
+              onPressed: onEdit, 
             ),
             IconButton(
               icon: const Icon(Icons.delete, color: Colors.red),
-              onPressed: onDelete, // Memicu fungsi hapus yang dikirim dari parent [cite: 122]
+              onPressed: onDelete, 
             ),
           ],
         ),
