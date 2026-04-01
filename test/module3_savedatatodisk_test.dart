ff76587c548e2931ba94a7c40a76ff2450c125c8
helgaathifah05@gmail.com <helgaathifah05@gmail.com>
2026-04-01 21:12:40 +0700

    upload module3 testing

---
+++ test/module3_savedatatodisk_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:logbook_app_077/features/logbook/log_controller.dart'; 
+
+void main() {
+  dynamic actual, expected;
+
+  group('Modul 3 - LogController Save to Disk (Test)', () {
+    late LogController controller;
+
+    setUp(() async {
+      // (1) setup (arrange, build)
+      // Inisialisasi mock storage 
+      SharedPreferences.setMockInitialValues({});
+      
+      // Buat objek LogController dengan username dummy
+      controller = LogController(username: "helga");
+      
+      // Tunggu sebentar agar loadFromDisk() di konstruktor selesai
+      await Future.delayed(const Duration(milliseconds: 100));
+    });
+
+    // Sesuai TC01 di Excel
+    test('addLog berhasil menambahkan log valid dan menyimpannya', () {
+      // (2) exercise (act, operate)
+      controller.addLog("Belajar Flutter", "Membuat test case Modul 3", "Akademik");
+
+      actual = controller.logsNotifier.value.length;
+      expected = 1;
+
+      // (3) verify (assert, check)
+      expect(actual, expected, reason: 'Expected $expected but got $actual');
+    });
+
+    // Sesuai TC02 di Excel 
+    test('addLog menolak menyimpan catatan jika title kosong', () {
+      // (2) exercise (act, operate)
+      controller.addLog("", "Deskripsi log tanpa judul", "Umum");
+
+      actual = controller.logsNotifier.value.length;
+      expected = 0; // Ekspektasinya log tidak bertambah (tetap 0) karena title kosong
+
+      // (3) verify (assert, check)
+      expect(actual, expected, reason: 'Expected $expected but got $actual');
+    });
+
+    // Sesuai TC03 di Excel 
+    test('addLog menolak menyimpan catatan jika description kosong', () {
+      // (2) exercise (act, operate)
+      controller.addLog("Judul Saja", "", "Umum");
+
+      actual = controller.logsNotifier.value.length;
+      expected = 0; // Ekspektasinya log tidak bertambah (tetap 0) karena desc kosong
+
+      // (3) verify (assert, check)
+      expect(actual, expected, reason: 'Expected $expected but got $actual');
+    });
+  });
+}
